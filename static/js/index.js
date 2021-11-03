3ee9ca6fdb1a3f4a7d5c338a15b98e263135ffc4
gretaivan <greta.ivan91@gmail.com>
2021-11-03 20:23:40 +0000

    installs packages, adds form and header, footer potential elements

---
+++ static/js/index.js
@@ -0,0 +1,40 @@
+const main = document.querySelector('main');
+
+const publicRoutes = ['#'];
+const privateRoutes = [];
+
+
+window.addEventListener('hashchange', updateContent);
+window.addEventListener('load', updateContent);
+
+
+function updateContent(){
+    const path = window.location.hash;
+    //console.log("window path: " + path)
+
+    //private path or not existing user
+    if (privateRoutes.includes(path) && !currentUser()){
+        window.location.hash = '#';
+        alert("You need to register or login!")
+    } else {
+        updateMain(path);
+    }
+}
+
+function updateMain(path) {
+    main.innerHTML = '';
+    if (path) {
+        switch(path){
+            case '#login':
+                renderLoginForm(); break;
+            case '#register':
+                renderRegisterForm(); break;
+            case '#habits':
+                renderUserPage(); break;
+            default:
+                render404(); break;
+        }
+    } else {
+        renderLoginForm();
+    }
+}
